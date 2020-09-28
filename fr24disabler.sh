b6a08c0d62566c2d0e0b7d97198511f875b1b431
Bruno Stelmaszyk <48010443+stelmaszyk@users.noreply.github.com>
2020-09-28 23:12:23 +0200

    Add files via upload

---
+++ fr24disabler.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$(whoami)" != "root" ];
+  then echo "You need to run this script as root"
+  exit 1;
+fi
+echo "Stopping FR24Feed Service"
+systemctl stop fr24feed
+sleep 1
+echo "OK"
+echo "==============================="
+echo "Disabling FR24Feed service"
+systemctl disable fr24feed
+sleep 1
+echo "ok"
+sleep 1
+echo "Looking for FR24Feed processes in the operating system. If found, killing:"
+killall fr24feed
+killall fr24feed-status
+sleep 1
+echo "Done. Fr24Feed service has been succesfully stopped and disabled. "
